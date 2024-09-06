61f0678b745d41b8000f571622786aa953c41575
rzz <1790146932@qq.com>
2024-09-06 14:24:05 +0800

    image_api

---
+++ api/image/image_delete.go
@@ -0,0 +1,5 @@
+package image
+
+import "github.com/gin-gonic/gin"
+
+func (i Image) ImageDelete(c *gin.Context) {}
---
+++ api/image/image_list.go
@@ -0,0 +1,7 @@
+package image
+
+import "github.com/gin-gonic/gin"
+
+func (i Image) ImageList(c *gin.Context) {
+
+}

25ff71d0434c3f70745a52c3744143deaeabf365
rzz <1790146932@qq.com>
2024-09-06 15:02:18 +0800

    image_api

--- api/image/image_delete.go
+++ api/image/image_delete.go
@@ -1,5 +1,31 @@
 package image
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog/global"
+	"blog/models"
+	"blog/models/res"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
-func (i Image) ImageDelete(c *gin.Context) {}
+func (i Image) ImageDelete(c *gin.Context) {
+	var req models.RemoveRequest
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		res.FailWithError(err, &req, c)
+		return
+	}
+	var imageList []models.ImageModel
+	count := global.DB.Find(&imageList, req.IDList).RowsAffected
+	if count == 0 {
+		res.FailWithMessage("图片不存在", c)
+		return
+	}
+	err = global.DB.Delete(&imageList).Error
+	if err != nil {
+		res.FailWithMessage("图片删除失败", c)
+		return
+	}
+	res.OkWithMessage(fmt.Sprintf("已删除 %d 张图片", count), c)
+
+}

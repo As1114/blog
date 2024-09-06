eb2c9c7f85d2876370f7d473443bcd3240ce34ed
rzz <1790146932@qq.com>
2024-09-06 14:20:42 +0800

    生成image表

--- flags/flags_db.go
+++ flags/flags_db.go
@@ -9,7 +9,8 @@ import (
 
 func DB(c *cli.Context) (err error) {
 	err = global.DB.Set("gorm:table_options", "ENGINE=InnoDB").
-		AutoMigrate(&models.UserModel{})
+		AutoMigrate(&models.UserModel{},
+			&models.ImageModel{})
 	if err != nil {
 		zap.L().Error("[ error ] 生成数据库表结构失败")
 		return nil

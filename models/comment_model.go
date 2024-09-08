afe334857c69ffca090aaab996dcfaaa70d68107
rzz <1790146932@qq.com>
2024-09-08 08:50:49 +0800

    comment_model,message_model

--- models/comment_model.go
+++ models/comment_model.go
@@ -5,10 +5,10 @@ type CommentModel struct {
 	SubComments        []*CommentModel `gorm:"foreignKey:ParentCommentID" json:"sub_comments"`
 	ParentCommentModel *CommentModel   `gorm:"foreignKey:ParentCommentID" json:"comment_model"`
 	ParentCommentID    *uint           `gorm:"comment:父评论id" json:"parent_comment_id"`
-	Content            string          `gorm:"size:256;comment:评论内容" json:"content"`
-	DiggCount          int             `gorm:"size:8;default:0;comment:点赞数" json:"digg_count"`
-	CommentCount       int             `gorm:"size:8;default:0;comment:子评论数" json:"comment_count"`
-	ArticleID          string          `gorm:"size:32;comment:文章id" json:"article_id"`
+	Content            string          `gorm:"comment:评论内容" json:"content"`
+	DiggCount          int             `gorm:"default:0;comment:点赞数" json:"digg_count"`
+	CommentCount       int             `gorm:"default:0;comment:子评论数" json:"comment_count"`
+	ArticleID          string          `gorm:"comment:文章id" json:"article_id"`
 	User               UserModel       `gorm:"comment:关联的用户" json:"user"`
 	UserID             uint            `gorm:"comment:关联用户的id" json:"user_id"`
 }

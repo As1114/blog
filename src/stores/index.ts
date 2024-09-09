9398d3b77eb60990cdb3a06d8e97c41c1bd10e73
rzz <1790146932@qq.com>
2024-09-09 22:08:48 +0800

    初始化

---
+++ src/stores/index.ts
@@ -0,0 +1,15 @@
+import {defineStore} from 'pinia'
+
+
+export const useStore = defineStore('counter', {
+    state() {
+        return {
+
+        }
+    },
+    actions: {
+    },
+    getters: {
+
+    }
+})

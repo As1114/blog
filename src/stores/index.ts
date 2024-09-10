8868ac268366e6e52c4a47231fc981e336f0325a
rzz <1790146932@qq.com>
2024-09-10 17:44:13 +0800

    store状态管理

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,13 +1,46 @@
 import {defineStore} from 'pinia'
 
+const theme: boolean = true // true light   false  dark
+const collapsed: boolean = false
+
 export const useStore = defineStore('counter', {
     state() {
         return {
+            theme: theme,
+            collapsed: collapsed,
         }
     },
     actions: {
+        setCollapsed(collapsed: boolean) {
+            this.collapsed = collapsed
+        },
+        setTheme(localTheme?: boolean) {
+            if (localTheme != undefined) {
+                this.theme = localTheme
+            } else {
+                this.theme = !this.theme
+            }
+            document.documentElement.style.colorScheme = this.themeString
+            document.body.setAttribute('arco-theme', this.themeString)
+            localStorage.setItem("theme", JSON.stringify(this.theme))
+        },
+        loadTheme() {
+            let val = localStorage.getItem("theme");
+            if (val === null) {
+                return
+            }
+            try {
+                this.theme = JSON.parse(val);
+                document.documentElement.style.colorScheme = this.themeString;
+                document.body.setAttribute('arco-theme', this.themeString);
+            } catch (e) {
+                return
+            }
+        }
     },
     getters: {
-
+        themeString(): "light" | "dark" {
+            return this.theme ? "light" : "dark"
+        },
     }
 })

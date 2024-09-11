313b51913b6b64563ae86593766fe8793d664a5a
rzz <1790146932@qq.com>
2024-09-11 08:12:59 +0800

    web_index初始化

---
+++ src/views/web/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="web_index">
+    <div class="web_index_header">
+      </div>
+    <main>
+        <div class="web_index_aside"></div>
+        <div class="web_index_content">
+          <router-view v-slot="{Component}">
+            <transition mode="out-in" name="fade">
+              <component :is="Component"></component>
+            </transition>
+          </router-view>
+        </div>
+      </main>
+    <div class="web_index_footer"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+
+</script>
+
+<style lang="scss">
+
+//过渡动画
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.fade-enter-active {
+  transform: translateX(-30px);
+  opacity: 0;
+}
+
+.fade-enter-to {
+  transform: translateX(0px);
+  opacity: 1;
+}
+
+.fade-leave-active, .fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+</style>

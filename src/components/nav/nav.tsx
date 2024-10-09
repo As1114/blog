43de105bc00a8c1ae8e2b6668fb9893cb9082951
rzz <1790146932@qq.com>
2024-10-09 23:21:06 +0800

    nav

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,9 +1,15 @@
-﻿import "./nav.css";
+﻿import { Navleft } from "../nav_left/nav_left";
+import { Navright } from "../nav_right/nav_right";
+import "./nav.css";
 export function Nav() {
   return (
     <div className="nav">
-      <div className="left">left</div>
-      <div className="right">right</div>
+      <div className="left">
+        <Navleft></Navleft>
+      </div>
+      <div className="right">
+        <Navright></Navright>
+      </div>
     </div>
   );
 }
---
+++ src/components/nav_left/nav_left.tsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import { Cover } from "@/components/ui/cover";
+import "./nav_left.css";
+export function Navleft() {
+  return (
+    <div className="nav_left">
+      <div className="slogan">
+        <Cover>NSXZ</Cover>
+      </div>
+    </div>
+  );
+}

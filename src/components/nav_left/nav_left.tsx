ddad2c807653a947446aeee0ef73697c2de99f0b
rzz <1790146932@qq.com>
2024-10-12 20:56:55 +0800

    web_demo

--- src/components/nav_left/nav_left.tsx
+++ src/components/nav_left/nav_left.tsx
@@ -6,7 +6,9 @@ export function Navleft() {
   return (
     <div className="nav_left">
       <div className="slogan">
-        <ConfettiDemo></ConfettiDemo>
+        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center font-semibold leading-none text-transparent">
+          NSXZ
+        </span>
       </div>
       <div className="function">
         <NavbarDemo></NavbarDemo>
